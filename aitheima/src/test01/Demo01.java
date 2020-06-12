e33afaf18016c296fbe2515e26828f186dea1697
ZHANGFENGBIN <zhang_fengbin@putlook.com>
2020-06-12 20:25:17 +0800

    第四次 输出多次你好世界

--- aitheima/src/test01/Demo01.java
+++ aitheima/src/test01/Demo01.java
@@ -11,8 +11,10 @@ public class Demo01 {
     public static void main(String[] args) {
 
         System.out.println("你好世界");
-
-
+        System.out.println("你好世界");
+        System.out.println("你好世界");
+        System.out.println("你好世界");
+        System.out.println("你好世界");
 
     }
 
